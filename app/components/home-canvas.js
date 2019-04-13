5eed8491f2945875125f21420d88950e16fab2f3
Benjamin Rosas <ben@aliencyb.org>
2019-04-13 01:11:47 -0500

    hover images

--- app/components/home-canvas.js
+++ app/components/home-canvas.js
@@ -69,9 +69,11 @@ function makeStage(height, width) {
   return stage
 }
 
-function buildImage(imageObj, x, y) {
+function buildImage(imageObj, name, x, y, visible = true) {
   return new Konva.Image({
     image: imageObj,
+    name,
+    visible,
     x,
     y
   })
@@ -87,14 +89,24 @@ function imagePromise(imageObj, src) {
 export default class HomeCanvasComponent extends Component {
   async setup() {
     const interiorImageObj = new Image()
+
+    const castCrewBtnImageObj = new Image()
     const castCrewImageObj = new Image()
+    const episodesBtnImageObj = new Image()
     const episodesImageObj = new Image()
+    const gamesBtnImageObj = new Image()
     const gamesImageObj = new Image()
+    const giveBackBtnImageObj = new Image()
     const giveBackImageObj = new Image()
+    const musicBtnImageObj = new Image()
     const musicImageObj = new Image()
+    const photosBtnImageObj = new Image()
     const photosImageObj = new Image()
+    const shopBtnImageObj = new Image()
     const shopImageObj = new Image()
+    const trailerBtnImageObj = new Image()
     const trailerImageObj = new Image()
+    const youtopiaBtnImageObj = new Image()
     const youtopiaImageObj = new Image()
 
     const bgLayer = makeLayer()
@@ -109,15 +121,60 @@ export default class HomeCanvasComponent extends Component {
       y: 0
     })
 
-    const castCrewImg = buildImage(castCrewImageObj, 1470, 1130)
-    const episodesImg = buildImage(episodesImageObj, 560, 1130)
-    const gamesImg = buildImage(gamesImageObj, 485, 640)
-    const giveBackImg = buildImage(giveBackImageObj, 1545, 1445)
-    const musicImg = buildImage(musicImageObj, 500, 1020)
-    const photosImg = buildImage(photosImageObj, 1730, 450)
-    const shopImg = buildImage(shopImageObj, 20, 920)
-    const trailerImg = buildImage(trailerImageObj, 850, 870)
-    const youtopiaImg = buildImage(youtopiaImageObj, 1462, 733)
+    const castCrewBtnImg = buildImage(
+      castCrewBtnImageObj,
+      'castCrew',
+      1390,
+      1060,
+      false
+    )
+    const castCrewImg = buildImage(castCrewImageObj, 'castCrew', 1470, 1130)
+    const episodesBtnImg = buildImage(
+      episodesBtnImageObj,
+      'episodes',
+      500,
+      1100,
+      false
+    )
+    const episodesImg = buildImage(episodesImageObj, 'episodes', 560, 1130)
+    const gamesBtnImg = buildImage(gamesBtnImageObj, 'games', 455, 600, false)
+    const gamesImg = buildImage(gamesImageObj, 'games', 485, 640)
+    const giveBackBtnImg = buildImage(
+      giveBackBtnImageObj,
+      'giveBack',
+      1520,
+      1410,
+      false
+    )
+    const giveBackImg = buildImage(giveBackImageObj, 'giveBack', 1545, 1445)
+    const musicBtnImg = buildImage(musicBtnImageObj, 'music', 470, 960, false)
+    const musicImg = buildImage(musicImageObj, 'music', 500, 1020)
+    const photosBtnImg = buildImage(
+      photosBtnImageObj,
+      'photos',
+      1610,
+      420,
+      false
+    )
+    const photosImg = buildImage(photosImageObj, 'photos', 1730, 450)
+    const shopBtnImg = buildImage(shopBtnImageObj, 'shop', 0, 850, false)
+    const shopImg = buildImage(shopImageObj, 'shop', 20, 910)
+    const trailerBtnImg = buildImage(
+      trailerBtnImageObj,
+      'trailer',
+      820,
+      820,
+      false
+    )
+    const trailerImg = buildImage(trailerImageObj, 'trailer', 850, 870)
+    const youtopiaBtnImg = buildImage(
+      youtopiaBtnImageObj,
+      'youtopia',
+      1370,
+      685,
+      false
+    )
+    const youtopiaImg = buildImage(youtopiaImageObj, 'youtopia', 1462, 733)
 
     function resizeFit() {
       fitStageIntoParentContainer(stage, 1600, 1920)
@@ -127,15 +184,36 @@ export default class HomeCanvasComponent extends Component {
 
     bgLayer.add(interiorImg)
 
+    await imagePromise(castCrewBtnImageObj, CAST_CREW_B_SRC)
     await imagePromise(castCrewImageObj, CAST_CREW_SRC)
+    await imagePromise(episodesBtnImageObj, EPISODES_B_SRC)
     await imagePromise(episodesImageObj, EPISODES_SRC)
+    await imagePromise(gamesBtnImageObj, GAMES_B_SRC)
     await imagePromise(gamesImageObj, GAMES_SRC)
+    await imagePromise(giveBackBtnImageObj, GIVE_BACK_B_SRC)
     await imagePromise(giveBackImageObj, GIVE_BACK_SRC)
+    await imagePromise(musicBtnImageObj, MUSIC_B_SRC)
     await imagePromise(musicImageObj, MUSIC_SRC)
+    await imagePromise(photosBtnImageObj, PHOTOS_B_SRC)
     await imagePromise(photosImageObj, PHOTOS_SRC)
+    await imagePromise(shopBtnImageObj, SHOP_B_SRC)
     await imagePromise(shopImageObj, SHOP_SRC)
+    await imagePromise(trailerBtnImageObj, TRAILER_B_SRC)
     await imagePromise(trailerImageObj, TRAILER_SRC)
+    await imagePromise(youtopiaBtnImageObj, YOUTOPIA_B_SRC)
     await imagePromise(youtopiaImageObj, YOUTOPIA_SRC)
+
+    const imageMap = {
+      castCrew: [castCrewImg, castCrewBtnImg],
+      episodes: [episodesImg, episodesBtnImg],
+      games: [gamesImg, gamesBtnImg],
+      giveBack: [giveBackImg, giveBackBtnImg],
+      music: [musicImg, musicBtnImg],
+      photos: [photosImg, photosBtnImg],
+      shop: [shopImg, shopBtnImg],
+      trailer: [trailerImg, trailerBtnImg],
+      youtopia: [youtopiaImg, youtopiaBtnImg]
+    }
 
     fgLayer.add(
       castCrewImg,
@@ -146,13 +224,50 @@ export default class HomeCanvasComponent extends Component {
       photosImg,
       shopImg,
       trailerImg,
-      youtopiaImg
+      youtopiaImg,
+      castCrewBtnImg,
+      episodesBtnImg,
+      gamesBtnImg,
+      giveBackBtnImg,
+      musicBtnImg,
+      photosBtnImg,
+      shopBtnImg,
+      trailerBtnImg,
+      youtopiaBtnImg
     )
 
     stage.add(bgLayer)
     stage.add(fgLayer)
 
     resizeFit()
+
+    fgLayer.on('mouseover', evt => {
+      const {
+        target: {
+          attrs: { name }
+        }
+      } = evt
+      const [originalImage, btnImage] = imageMap[name]
+
+      document.body.style.cursor = 'pointer'
+      originalImage.hide()
+      btnImage.show()
+      stage.draw()
+    })
+
+    fgLayer.on('mouseout', evt => {
+      const {
+        target: {
+          attrs: { name }
+        }
+      } = evt
+      const [originalImage, btnImage] = imageMap[name]
+
+      document.body.style.cursor = 'default'
+      btnImage.hide()
+      originalImage.show()
+      stage.draw()
+    })
 
     window.addEventListener('resize', resizeFit)
   }
