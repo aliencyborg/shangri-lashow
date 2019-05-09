71d1e07a43e2aea5e1fa4f77e32d354ac56460e9
Benjamin Rosas <ben@aliencyb.org>
2019-05-09 02:02:41 -0500

    image action

--- app/controllers/photos.js
+++ app/controllers/photos.js
@@ -5,8 +5,16 @@ import { toTitleCase } from 'shangri-lashow/util/strings'
 export default class PhotosController extends Controller {
   photoSources = images.photos()
 
-  items = Object.keys(this.photoSources).map(key => ({
-    alt: toTitleCase(key),
-    src: this.photoSources[key]
-  }))
+  items = Object.keys(this.photoSources).map(key => {
+    const image = new Image()
+    image.src = this.photoSources[key]
+    const { height, width } = image
+
+    return {
+      alt: toTitleCase(key),
+      src: this.photoSources[key],
+      h: height,
+      w: width
+    }
+  })
 }
