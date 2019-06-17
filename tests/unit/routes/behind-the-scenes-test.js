7c372d8dcf9568d505e385907f63e869febafc66
Benjamin Rosas <ben@aliencyb.org>
2019-06-17 00:37:47 -0500

    test stubs

--- tests/unit/routes/behind-the-scenes-test.js
+++ tests/unit/routes/behind-the-scenes-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit'
 import { setupTest } from 'ember-qunit'
 
-module('Unit | Route | trailer', function(hooks) {
+module('Unit | Route | behind-the-scenes', function(hooks) {
   setupTest(hooks)
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:trailer')
+    let route = this.owner.lookup('route:behind-the-scenes')
     assert.ok(route)
   })
 })
