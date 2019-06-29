06e6cd97a3d4f4a25f1aec9f76c329e20859d783
Benjamin Rosas <ben@aliencyb.org>
2019-06-28 22:59:09 -0500

    randomize photos; fix top/bottom height

--- app/components/photo-viewer.js
+++ app/components/photo-viewer.js
@@ -4,14 +4,20 @@ import images from 'shangri-lashow/util/images'
 import { imagePromise } from 'shangri-lashow/util/canvas-utils'
 import { toTitleCase } from 'shangri-lashow/util/strings'
 
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5)
+}
+
 export default class PhotoViewerComponent extends Component {
   photoSources = images.photos()
   imageKeys = Object.keys(this.photoSources)
   @tracked items = []
 
   setup = async () => {
+    const shuffledImageKeys = shuffle(this.imageKeys)
+
     this.items = await Promise.all(
-      this.imageKeys.map(async key => {
+      shuffledImageKeys.map(async key => {
         const image = new Image()
 
         await imagePromise(image, this.photoSources[key])
