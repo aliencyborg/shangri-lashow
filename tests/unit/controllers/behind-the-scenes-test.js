0a23fcdecc21124320d7a1bdf42cb12749571db0
Benjamin Rosas <ben@aliencyb.org>
2019-06-28 20:02:51 -0500

    updates

--- tests/unit/controllers/behind-the-scenes-test.js
+++ tests/unit/controllers/behind-the-scenes-test.js
@@ -1,12 +1,12 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit'
+import { setupTest } from 'ember-qunit'
 
 module('Unit | Controller | behind-the-scenes', function(hooks) {
-  setupTest(hooks);
+  setupTest(hooks)
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:behind-the-scenes');
-    assert.ok(controller);
-  });
-});
+    let controller = this.owner.lookup('controller:behind-the-scenes')
+    assert.ok(controller)
+  })
+})
